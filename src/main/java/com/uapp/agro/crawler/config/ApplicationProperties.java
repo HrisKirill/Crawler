4ff73526cdf8a89257919170378dfeedbe06f0c1
Khrystoforov Kyrylo <kyrylo.khrystoforov@uapp-llc.com>
2024-10-12 17:17:53 +0300

    add thread count to application properties

--- src/main/java/com/uapp/agro/crawler/config/ApplicationProperties.java
+++ src/main/java/com/uapp/agro/crawler/config/ApplicationProperties.java
@@ -4,12 +4,22 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 @ConfigurationProperties("application")
 @RequiredArgsConstructor
 @Data
 public class ApplicationProperties {
     private final ImageProperties imageProperties;
+    private final ThreadProperties threadProperties;
 
-    public record ImageProperties(String folderPath, Long sizeBeforeCompressedKb) {
+    public record ImageProperties(
+            String folderPath,
+            Long sizeBeforeCompressedKb,
+            List<String> availableFormats) {
+    }
+
+    public record ThreadProperties(Integer maxProducerCount,
+                                   Integer maxConsumerCount) {
     }
 }
--- src/main/java/com/uapp/agro/crawler/image/service/impl/ImageScraperServiceImpl.java
+++ src/main/java/com/uapp/agro/crawler/image/service/impl/ImageScraperServiceImpl.java
@@ -5,7 +5,6 @@ import com.uapp.agro.crawler.image.service.ImageInfoService;
 import com.uapp.agro.crawler.image.service.ImageScraperService;
 import com.uapp.agro.crawler.image.service.consumer.ImageScraperConsumer;
 import com.uapp.agro.crawler.image.service.producer.ImageScraperProducer;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StopWatch;
@@ -17,41 +16,52 @@ import java.util.stream.IntStream;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class ImageScraperServiceImpl implements ImageScraperService {
     private final ApplicationProperties properties;
+    private final Integer maxProducerThreadCount;
+    private final Integer maxConsumerThreadCount;
     private final BlockingQueue<String> images = new LinkedBlockingQueue<>();
     private final Set<String> visitedUrls = ConcurrentHashMap.newKeySet();
     private final Set<String> visitedImages = ConcurrentHashMap.newKeySet();
     private final ConcurrentSkipListSet<String> urlQueue = new ConcurrentSkipListSet<>();
-    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+    private final ExecutorService executorService;
     private final ImageInfoService infoService;
     private final StopWatch stopWatch = new StopWatch();
+
+    public ImageScraperServiceImpl(ApplicationProperties properties, ImageInfoService infoService) {
+        this.properties = properties;
+        this.maxProducerThreadCount = properties.getThreadProperties().maxProducerCount();
+        this.maxConsumerThreadCount = properties.getThreadProperties().maxConsumerCount();
+        this.executorService = Executors.newFixedThreadPool(maxProducerThreadCount + maxConsumerThreadCount);
+        this.infoService = infoService;
+    }
 
     @Override
     public void startScraping(String startUrl) {
         stopWatch.start();
-        final Long minImageSize = properties.getImageProperties().sizeBeforeCompressedKb();
-        final String folderPath = properties.getImageProperties().folderPath();
-        final int producerCount = 1;
-        final int consumerCount = 3;
-
-        AtomicInteger producersCount = new AtomicInteger(producerCount);
-        executorService.submit(new ImageScraperProducer(images, startUrl, minImageSize, urlQueue, visitedUrls, visitedImages, producersCount, executorService));
+        Long minImageSize = properties.getImageProperties().sizeBeforeCompressedKb();
+        String folderPath = properties.getImageProperties().folderPath();
+        int startProducerThreadCount = 1;
+        AtomicInteger producersCount = new AtomicInteger(startProducerThreadCount);
+        executorService.submit(new ImageScraperProducer(images, startUrl, minImageSize, urlQueue, visitedUrls, visitedImages, producersCount, executorService, maxProducerThreadCount));
 
 
-        CompletableFuture[] futures = IntStream.rangeClosed(0, consumerCount)
+        CompletableFuture[] futures = IntStream.rangeClosed(0, maxConsumerThreadCount)
                 .mapToObj(i -> CompletableFuture.runAsync(
-                        new ImageScraperConsumer(images, folderPath, infoService, producersCount),
+                        new ImageScraperConsumer(images,
+                                folderPath,
+                                infoService,
+                                producersCount,
+                                properties.getImageProperties().availableFormats()),
                         executorService
                 ))
                 .toArray(CompletableFuture[]::new);
 
         CompletableFuture.allOf(futures)
                 .whenComplete((unused, throwable) -> {
+                    shutdownExecutorService();
                     stopWatch.stop();
                     log.info("Execution time: {} ms", stopWatch.getTotalTimeMillis());
-                    shutdownExecutorService();
                 });
     }
 
@@ -59,7 +69,7 @@ public class ImageScraperServiceImpl implements ImageScraperService {
     private void shutdownExecutorService() {
         executorService.shutdown();
         try {
-            boolean terminated = executorService.awaitTermination(30, TimeUnit.SECONDS);
+            boolean terminated = executorService.awaitTermination(3, TimeUnit.SECONDS);
             if (!terminated) {
                 log.info("Forcing shutdown...");
                 executorService.shutdownNow();
