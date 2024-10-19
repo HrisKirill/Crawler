76db751fbd4968cbb35617e0ce7ef5023b704c5b
Khrystoforov Kyrylo <kyrylo.khrystoforov@uapp-llc.com>
2024-10-19 12:20:18 +0300

    lock db for image checking and inserting

--- src/main/java/com/uapp/agro/crawler/image/service/impl/ImageInfoServiceImpl.java
+++ src/main/java/com/uapp/agro/crawler/image/service/impl/ImageInfoServiceImpl.java
@@ -6,23 +6,27 @@ import com.uapp.agro.crawler.image.repository.ImageInfoRepository;
 import com.uapp.agro.crawler.image.service.ImageInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @RequiredArgsConstructor
 public class ImageInfoServiceImpl implements ImageInfoService {
     private final ImageInfoRepository imageInfoRepository;
+    private final Lock lock = new ReentrantLock();
 
     @Override
-    @Transactional
     public ImageInfo createIfNotExists(ImageCreateDto dto) {
-        Optional<ImageInfo> optionalImageInfo = imageInfoRepository.findByOriginalUrl(dto.getOriginalUrl());
-        if (optionalImageInfo.isEmpty()) {
-            ImageInfo imageInfo = new ImageInfo(dto.getOriginalUrl(), dto.getFilePath());
-            return imageInfoRepository.save(imageInfo);
+        try {
+            lock.lock();
+            return imageInfoRepository.findByOriginalUrl(dto.getOriginalUrl())
+                    .orElseGet(() -> {
+                        ImageInfo imageInfo = new ImageInfo(dto.getOriginalUrl(), dto.getFilePath());
+                        return imageInfoRepository.save(imageInfo);
+                    });
+        } finally {
+            lock.unlock();
         }
-        return optionalImageInfo.get();
     }
 }
