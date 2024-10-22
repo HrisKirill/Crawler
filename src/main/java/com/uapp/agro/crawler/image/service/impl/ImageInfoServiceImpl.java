0072dd36bbbb370d2620d5143c7967406ed117df
Khrystoforov Kyrylo <kyrylo.khrystoforov@uapp-llc.com>
2024-10-22 15:04:58 +0300

    change ReentrantLock to db lock

--- src/main/java/com/uapp/agro/crawler/image/service/impl/ImageInfoServiceImpl.java
+++ src/main/java/com/uapp/agro/crawler/image/service/impl/ImageInfoServiceImpl.java
@@ -4,29 +4,29 @@ import com.uapp.agro.crawler.image.dto.ImageCreateDto;
 import com.uapp.agro.crawler.image.model.ImageInfo;
 import com.uapp.agro.crawler.image.repository.ImageInfoRepository;
 import com.uapp.agro.crawler.image.service.ImageInfoService;
+import jakarta.persistence.LockModeType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ImageInfoServiceImpl implements ImageInfoService {
     private final ImageInfoRepository imageInfoRepository;
-    private final Lock lock = new ReentrantLock();
 
     @Override
+    @Transactional
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     public ImageInfo createIfNotExists(ImageCreateDto dto) {
-        try {
-            lock.lock();
-            return imageInfoRepository.findByOriginalUrl(dto.getOriginalUrl())
-                    .orElseGet(() -> {
-                        ImageInfo imageInfo = new ImageInfo(dto.getOriginalUrl(), dto.getFilePath());
-                        return imageInfoRepository.save(imageInfo);
-                    });
-        } finally {
-            lock.unlock();
-        }
+        return imageInfoRepository.findByOriginalUrl(dto.getOriginalUrl())
+                .orElseGet(() -> {
+                    ImageInfo imageInfo = new ImageInfo(dto.getOriginalUrl(), dto.getFilePath());
+                    log.info("Save image: {}", imageInfo.getOriginalUrl());
+                    return imageInfoRepository.save(imageInfo);
+                });
+
     }
 }
